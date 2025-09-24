690dc2bc55d45d17434a93b7e2129ff1ad0476a1
ppael06 <rafaelputra2006@gmail.com>
2025-09-24 10:22:18 +0700

    71241092

--- pengembalian/pengembalian.py
+++ pengembalian/pengembalian.py
@@ -7,10 +7,15 @@ def process_pengembalian(buku, mahasiswa):
         # 1. Validasi mahasiswa
         # TODO: melakukan pengecekan apakah nama mahasiswa ada dalam dictionary buku, kemudian
         #    return (judul, "Gagal", "Mahasiswa tidak terdaftar")
+        if nama not in mahasiswa:
+            return (judul, "Gagal", "Mahasiswa tidak terdaftar")
 
         # 2. Validasi judul buku
         # TODO: melakukan pengecekan apakah judul ada dalam dictionary buku, kemudian
         #    return (judul, "Gagal", "Judul tidak ditemukan")
+        if judul not in buku:
+            return (judul, "Gagal", "Judul tidak ditemukan")
+            
 
         # 3. TODO: cek apakah buku tercatat sedang dipinjam oleh mahasiswa
         # hint: gunakan if judul in mahasiswa[nama].get("pinjaman", [])
@@ -18,9 +23,20 @@ def process_pengembalian(buku, mahasiswa):
         # lalu tambahkan stok buku +1
         # jika status buku sebelumnya "Kosong", ubah menjadi "Tersedia"
             # return (judul, "Berhasil", None)
+        if judul in mahasiswa[nama].get("pinjaman", []):
+            mahasiswa[nama]["pinjaman"].remove(judul)
+            buku[judul]["stok"] += 1
+            
+        if buku[judul]["status"] == "Kosong":
+            buku[judul]["status"] = "Tersedia"
+            return(judul, "Berhasil", None)
+            
+            
+            
+            
 
         # 4. Jika tidak ada di daftar pinjaman
-        return (judul, "Gagal", "Tidak tercatat sebagai pinjaman")
+        # return (judul, "Gagal", "Tidak tercatat sebagai pinjaman")
 
     for nama, data in mahasiswa.items():
         hasil_berhasil, hasil_gagal = [], []
